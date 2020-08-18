d4a5f91126b6cb0ed0a8d278a1838f4b09cd4749
CDX <1138224907@qq.com>
2020-08-18 10:37:34 +0800

    set

---
+++ src/main/java/com/other/collection/set/MyLinkedHashSet.java
@@ -0,0 +1,11 @@
+package com.other.collection.set;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class MyLinkedHashSet {
+    public static void main(String[] args) {
+        Set<Integer> set = new LinkedHashSet<>();
+    }
+}
---
+++ src/main/java/com/other/collection/set/MyTreeSet.java
@@ -0,0 +1,11 @@
+package com.other.collection.set;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class MyTreeSet {
+    public static void main(String[] args) {
+        Set<Integer> set = new TreeSet<>();
+    }
+}
